d4363a5dd62a1d8f8ef7e7f83e0041bd66c67a55
LorenaMartiins <lorena.martins.farias@hotmail.com>
2021-12-06 21:32:29 -0300

    python

---
+++ Aula_37/Desafio_37/exercicio_01.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+planilha = pd.read_excel('Cotação dolár.xlsx')
+dolar = planilha['VALOR']
+
+plt.title("Cotação do Dolár - Jan/Nov")
+
+plt.ylabel("Valor do dolar")
+plt.xlabel("Mês")
+
+plt.plot(dolar, marker='D', linestyle='dotted', color='#696969')
+
+plt.show()
+
+os.system("pause")
